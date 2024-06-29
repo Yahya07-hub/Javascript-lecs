6af65653396ece7e03d8e14d1b739c9df428eed6
Yahya Bhabay <yahyabhabay07@Yahyas-MacBook-Air.local>
2024-06-29 22:02:27 +0530

    updated

--- advance_one/promise.js
+++ advance_one/promise.js
@@ -1,7 +1,7 @@
 const promiseOne = new Promise((resolve, rej) => {
     setTimeout(() => {
         resolve()
-        }, 1000)
+    }, 1000)
 })
 promiseOne.then(() => {
     console.log('promise1 consumed')
@@ -12,14 +12,14 @@ const promiseTwo = new Promise((resolve, rej) => {
         console.log('Async Task 2 Completed')
         resolve()
     }, 2000)
-}).then(()=>{
+}).then(() => {
     console.log('promise2 consumed')
 })
 
-const promise3 = new Promise((resolve,rej) => {
+const promise3 = new Promise((resolve, rej) => {
     setTimeout(() => {
-        resolve({username: 'Yahya Bhabay', Email: 'yahyabhabay07@gmail.com'})
-        }, 3000)
+        resolve({ username: 'Yahya Bhabay', Email: 'yahyabhabay07@gmail.com' })
+    }, 3000)
 }).then((data) => {
     console.log(data)
 })
@@ -28,41 +28,56 @@ const promise4 = new Promise((resolve, reject) => {
     let error = false
     setTimeout(() => {
 
-        if(!error){
-            resolve({username: 'Yahya', Email: 'yahyabhabay07@gmail.com', InstaId: 'Yahyaaaaa.21'})
+        if (!error) {
+            resolve({ username: 'Yahya', Email: 'yahyabhabay07@gmail.com', InstaId: 'Yahyaaaaa.21' })
         }
-        else{
+        else {
             reject('Error: something went wrong')
         }
     }, 4000)
 }).then((userdata) => {
-console.log(userdata)
+    console.log(userdata)
     return userdata.InstaId
-}).then((userdata) => console.log(userdata))
-.catch((error) => console.log(error))
-.finally(() => console.log('Promise is Either resolved or rejected'))
+}).then((username) => console.log(username))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('Promise is Either resolved or rejected'))
 
 
 const promiseFive = new Promise((resolve, reject) => {
     let error = true
     setTimeout(() => {
 
-        if(!error){
-            resolve({username: 'Araf', Password: '123456'})
+        if (!error) {
+            resolve({ username: 'Araf', Password: '123456' })
         }
-        else{
+        else {
             reject('Error: server not working')
         }
     }, 5000)
 })
 
-consumePromiseFive = async () => {try {
-    const response = await promiseFive 
-    console.log(response.username)
-} catch (error) {
-    console.log(error)
-}
+consumePromiseFive = async () => {
+    try {
+        const response = await promiseFive
+        console.log(response.username)
+    } catch (error) {
+        console.log(error)
+    }
 
 }
 
 consumePromiseFive()
+
+getAllUsers = async () => {
+    try {
+        const userData = fetch('https://jsonplaceholder.typicode.com/users')
+        const response = await userData
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log("E: ", error)
+    }
+   
+}
+
+getAllUsers()
